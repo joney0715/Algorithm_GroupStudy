4d4a9ddea597fc22c9881d353b9d9ce8cd99db48
joney0715 <joney0715@gmail.com>
2022-08-02 22:56:59 +0900

    0803 study

---
+++ joney0715/0803/personal_BOJ_10830_행렬 제곱.py
@@ -0,0 +1,59 @@
+# 입력 값
+N, B = map(int, input().split())
+matrix = []
+for _ in range(N):
+    m = list(map(int, input().split()))
+    matrix.append(m)
+
+# 행렬의 곱
+def multiple(matrix_a, matrix_b):
+    # 곱 결과 초기화
+    matrix_c = []
+
+    # matrix_a의 행
+    for a in range(N):
+        # 계산 결과의 행 초기화
+        result = []
+        # matrix_b의 열
+        for b in range(N):
+            s = 0
+            # matrix_a의 열, matrix_b의 행           
+            for c in range(N):
+                # 각 요소의 곱
+                s += matrix_a[a][c] * matrix_b[c][b]
+            # 1000으로 나눈 나머지 (문제의 요구사항)
+            s = s % 1000
+            #계산 결과의 행
+            result.append(s)
+        matrix_c.append(result)
+    return matrix_c
+
+# 행의 제곱 계산
+def divide(matrix, B):
+    # 지수가 1인 경우
+    if B == 1:
+        # 지수가 1인 경우도 1000으로 나눠야 하는거 주의
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                matrix[i][j] = matrix[i][j] % 1000
+        return matrix
+
+    # 지수를 2로 나눠서 계산
+    # A^(a+b) = A^a * A^b
+    a = divide(matrix, B//2)
+    # 지수가 홀수인 경우
+    # A^(2a +1) = A^a * A^a * A
+    if B % 2:        
+        matrix_c = multiple(a, a)
+        matrix_c = multiple(matrix_c, matrix)
+        return matrix_c
+    # 짝수인 경우
+    # A^(2a +1) = A^a * A^a
+    else:
+        matrix_c = multiple(a, a)
+        return matrix_c
+    
+# 결과 출력
+matrix_c = divide(matrix, B)
+for row in matrix_c:
+    print(*row) # 언패킹 응용
---
+++ joney0715/0803/personal_BOJ_2800_괄호 제거.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+
+Input = list(input())
+
+# 괄호 페어를 확인 하기 위한 스택
+stack = []
+# 괄호가 있는 인덱스를 저장하기 위한 리스트
+pair = []
+for idx, char in enumerate(Input):   
+    # 괄호가 열리면 스택에 추가
+    if char == '(':
+        stack.append(idx)
+    # 괄호가 닫히면 스택에서 빼면서
+    # 인덱스 정보 저장
+    if char == ')':
+        pair.append([stack.pop(), idx])
+
+# 결과를 set으로 하는 이유
+# 리스트로 하면 ((1+2)) 같은 경우에 계산 안됨
+answer = set()
+for i in range(1,len(pair)+1):
+    # 없애려는 괄호의 조합 계산
+    for comb in list(combinations(pair, i)):
+        # 원본 수식의 얕은 복사
+        Input_c = Input[:]
+        # 괄호가 있는 경우 빈 문자열로 바꾸기
+        for j, k in comb:
+            Input_c[j] = ''
+            Input_c[k] = ''
+        answer.add(''.join(Input_c))
+        
+# 리스트로 변환
+answer = list(answer)
+# 사전순으로 정렬 (문제의 요구사항)
+answer.sort()
+for a in answer:
+    print(a)
