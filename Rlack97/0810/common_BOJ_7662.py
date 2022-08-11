d8fc25d327f26153c6d6747e1afa6b25ba149300
Rlack97 <amalilith97@gmail.com>
2022-08-11 17:46:44 +0900

    0811

--- Rlack97/0810/common_BOJ_7662.py
+++ Rlack97/0810/common_BOJ_7662.py
@@ -18,17 +18,16 @@ for _ in range(t):               # 테스트 케이스 수만큼 반복
             heapq.heappush(maxHeap,(-int(num),i))           # 최댓값의 경우 -를 붙여준 후, 출력할 때 다시 -를 사용한다.
             visited[i] = True      # 해당 순서의 visited 리스트를 True로 변경              
 
-
         else:     # 앞부분이 D일 경우, 1이면 최댓값을, -1이면 최솟값을 삭제한다
             if num == '-1':
                 while minHeap and not visited[minHeap[0][1]]:          
                     heapq.heappop(minHeap)
-                    #최소치의 삽입 순서를 인덱스로 하는 visited 리스트의 요소가 False일 때, 즉 해당 값을 방문한 적이 있을 때
+                    #최소치의 삽입 순서를 인덱스로 하는 visited 리스트의 요소가 False일 때, 즉 해당 방문 값이 삭제되었을 때
                     #최솟값을 제거
                 if minHeap:
                     visited[minHeap[0][1]] = False
                     heapq.heappop(minHeap)
-                    # 만약 방문한 적이 없으면, visited 리스트 값을 False로 바꾸고
+                    # 만약 방문값이 True 상태라면, visited 리스트 값을 False로 바꾸고
                     # 최솟값을 제거
             else:
                 while maxHeap and not visited[maxHeap[0][1]]:
@@ -43,6 +42,7 @@ for _ in range(t):               # 테스트 케이스 수만큼 반복
     while maxHeap and not visited[maxHeap[0][1]]:
         heapq.heappop(maxHeap)
         # 최댓값 큐 혹은 최솟값 큐 어느 한 쪽에서라도 그 값이 제거되었으면, 반대쪽 큐에서도 제거한다.
+
     if not minHeap or not maxHeap:
         print("EMPTY")
         # 이때, 어느 쪽이던 큐가 비어있다면 empty 출력
