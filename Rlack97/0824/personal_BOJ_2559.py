84bed039d3c46bc09ddd1a8c14027cf11131224b
Rlack97 <amalilith97@gmail.com>
2022-08-27 20:55:41 +0900

    commonproblems

--- Rlack97/0824/personal_BOJ_2559.py
+++ Rlack97/0824/personal_BOJ_2559.py
@@ -5,14 +5,18 @@ C = list(map(int, input().split()))
 added_temperature = 0
 for i in range(K):
     added_temperature += C[i]
+    # 지정된 일자 K만큼의 온도의 합을 구함
 
 max_temperature = added_temperature
+    #  초기 최대값 지정
 
 for j in range(N-K):
     added_temperature -= C[j] - C[j + K]
+    # 한 칸씩 진행
 
     if added_temperature >= max_temperature:
         max_temperature = added_temperature
+    # 최대값 갱신
 
 
 
