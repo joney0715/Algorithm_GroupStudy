b0d25937e7a78ce7d9cc250e227a1b38a431c174
yjp8842 <yjp20183029@gmail.com>
2022-09-06 21:33:51 +0900

    code

---
+++ yjp8842/0907/personal_BOJ_18126_너구리 구구.py
@@ -0,0 +1,64 @@
+# 예시출력1만 고려한 답안
+""" N = int(input())
+arr = []
+for _ in range(N - 1):
+    arr.append(list(map(int, input().split())))
+
+rooms = [[] for _ in range(N + 1)]
+road_len = [[] for _ in range(N + 1)]
+for i in range(len(arr)):
+    rooms[arr[i][0]].append(arr[i][1])
+    road_len[arr[i][1]].append(arr[i][2])
+
+cnt = 0    
+for i in rooms[1]:
+    cnt += road_len[i][0]
+    if rooms[i]:
+        j = max(rooms[i])
+        cnt += road_len[j][0]
+        
+print(cnt) """
+
+
+
+# 풀이 참고 답안
+
+N = int(input())
+rooms = [[] for _ in range(N + 1)]
+
+# 각 노드에서 갈 수 있는 노드를 모두 저장
+for i in range(N - 1):
+    A, B, C = map(int, input().split())
+    rooms[A].append([B, C])
+    rooms[B].append([A, C])
+
+visited = [0] * (N + 1)
+
+# 재귀 깊이 때문에 python 내에서 스택을 선언
+# (시스템 스택 사용 X)
+stack = [[1, 0]]
+
+result = 0
+while stack:
+    v, l = stack.pop()
+    visited[v] = 1
+	
+    # leaf node 인지 판별
+    flag = False
+    # 현재 v 노드가 갈 수 있는 곳이 한곳이라도 있다면 leaf 노드가 아님
+    for tmpv, tmpl in rooms[v]:
+        if visited[tmpv] == 0:
+            flag = True
+	
+    # leaf node가 아닌 경우, 스택에 저장
+    if flag == True:
+        if rooms[v]:
+            for nv, nl in rooms[v]:
+                if visited[nv] == 0:
+                    stack.append([nv, l + nl])
+    # leaf node인 경우, 최대값 갱신
+    else:
+        if l > result:
+            result = l
+
+print(result)
